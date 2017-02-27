4ebefb2c31642227838b10f9497582380fcd31cd
balraj G <balraj G>
2017-02-27 22:24:46 +0530

    revision on 27/2/2017

---
+++ Ipa/Custom Views/MyEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Ipa
+{
+	public class MyEntry : Entry
+	{
+		public static readonly BindableProperty BorderColorProperty = 
+			BindableProperty.Create<MyEntry, Color> (p => p.BorderColor, Color.White);
+
+		public Color BorderColor {
+			get { return (Color)GetValue (BorderColorProperty); }
+			set { SetValue (BorderColorProperty, value); }
+		}
+	}
+}
+
+
